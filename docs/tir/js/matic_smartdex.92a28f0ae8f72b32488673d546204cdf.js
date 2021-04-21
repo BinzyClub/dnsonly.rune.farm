49ce5cdbf3b3232f1bc3ad1d3a21b49859f59812
Binzy <xbinzy@protonmail.com>
2021-04-20 18:22:01 -0700

    build: Binzy doz it

---
+++ docs/tir/js/matic_smartdex.92a28f0ae8f72b32488673d546204cdf.js
@@ -0,0 +1,74 @@
+/*!
+* YieldFarming
+* Boilerplate for a Static website using EJS and SASS
+* https://yieldfarming.info
+* @author Jongseung Lim -- https://yieldfarming.info
+* Copyright 2021. MIT Licensed.
+*/
+
+
+$(function() {
+  consoleInit();
+  start(main);
+});
+
+const SMARTDEX_STAKING_ABI = [{"inputs":[{"internalType":"address","name":"_rewardsDistribution","type":"address"},{"internalType":"address","name":"_rewardsToken","type":"address"},{"internalType":"address","name":"_stakingToken","type":"address"}],"payable":false,"stateMutability":"nonpayable","type":"constructor"},{"anonymous":false,"inputs":[{"indexed":false,"internalType":"uint256","name":"reward","type":"uint256"}],"name":"RewardAdded","type":"event"},{"anonymous":false,"inputs":[{"indexed":true,"internalType":"address","name":"user","type":"address"},{"indexed":false,"internalType":"uint256","name":"reward","type":"uint256"}],"name":"RewardPaid","type":"event"},{"anonymous":false,"inputs":[{"indexed":true,"internalType":"address","name":"user","type":"address"},{"indexed":false,"internalType":"uint256","name":"amount","type":"uint256"}],"name":"Staked","type":"event"},{"anonymous":false,"inputs":[{"indexed":true,"internalType":"address","name":"user","type":"address"},{"indexed":false,"internalType":"uint256","name":"amount","type":"uint256"}],"name":"Withdrawn","type":"event"},{"constant":true,"inputs":[{"internalType":"address","name":"account","type":"address"}],"name":"balanceOf","outputs":[{"internalType":"uint256","name":"","type":"uint256"}],"payable":false,"stateMutability":"view","type":"function"},{"constant":true,"inputs":[{"internalType":"address","name":"account","type":"address"}],"name":"earned","outputs":[{"internalType":"uint256","name":"","type":"uint256"}],"payable":false,"stateMutability":"view","type":"function"},{"constant":false,"inputs":[],"name":"exit","outputs":[],"payable":false,"stateMutability":"nonpayable","type":"function"},{"constant":false,"inputs":[],"name":"getReward","outputs":[],"payable":false,"stateMutability":"nonpayable","type":"function"},{"constant":true,"inputs":[],"name":"getRewardForDuration","outputs":[{"internalType":"uint256","name":"","type":"uint256"}],"payable":false,"stateMutability":"view","type":"function"},{"constant":true,"inputs":[],"name":"lastTimeRewardApplicable","outputs":[{"internalType":"uint256","name":"","type":"uint256"}],"payable":false,"stateMutability":"view","type":"function"},{"constant":true,"inputs":[],"name":"lastUpdateTime","outputs":[{"internalType":"uint256","name":"","type":"uint256"}],"payable":false,"stateMutability":"view","type":"function"},{"constant":false,"inputs":[{"internalType":"uint256","name":"reward","type":"uint256"}],"name":"notifyRewardAmount","outputs":[],"payable":false,"stateMutability":"nonpayable","type":"function"},{"constant":true,"inputs":[],"name":"periodFinish","outputs":[{"internalType":"uint256","name":"","type":"uint256"}],"payable":false,"stateMutability":"view","type":"function"},{"constant":true,"inputs":[],"name":"rewardPerToken","outputs":[{"internalType":"uint256","name":"","type":"uint256"}],"payable":false,"stateMutability":"view","type":"function"},{"constant":true,"inputs":[],"name":"rewardPerTokenStored","outputs":[{"internalType":"uint256","name":"","type":"uint256"}],"payable":false,"stateMutability":"view","type":"function"},{"constant":true,"inputs":[],"name":"rewardRate","outputs":[{"internalType":"uint256","name":"","type":"uint256"}],"payable":false,"stateMutability":"view","type":"function"},{"constant":true,"inputs":[{"internalType":"address","name":"","type":"address"}],"name":"rewards","outputs":[{"internalType":"uint256","name":"","type":"uint256"}],"payable":false,"stateMutability":"view","type":"function"},{"constant":true,"inputs":[],"name":"rewardsDistribution","outputs":[{"internalType":"address","name":"","type":"address"}],"payable":false,"stateMutability":"view","type":"function"},{"constant":true,"inputs":[],"name":"rewardsDuration","outputs":[{"internalType":"uint256","name":"","type":"uint256"}],"payable":false,"stateMutability":"view","type":"function"},{"constant":true,"inputs":[],"name":"rewardsToken","outputs":[{"internalType":"contract IERC20","name":"","type":"address"}],"payable":false,"stateMutability":"view","type":"function"},{"constant":false,"inputs":[{"internalType":"uint256","name":"amount","type":"uint256"}],"name":"stake","outputs":[],"payable":false,"stateMutability":"nonpayable","type":"function"},{"constant":false,"inputs":[{"internalType":"uint256","name":"amount","type":"uint256"},{"internalType":"uint256","name":"deadline","type":"uint256"},{"internalType":"uint8","name":"v","type":"uint8"},{"internalType":"bytes32","name":"r","type":"bytes32"},{"internalType":"bytes32","name":"s","type":"bytes32"}],"name":"stakeWithPermit","outputs":[],"payable":false,"stateMutability":"nonpayable","type":"function"},{"constant":true,"inputs":[],"name":"stakingToken","outputs":[{"internalType":"contract IERC20","name":"","type":"address"}],"payable":false,"stateMutability":"view","type":"function"},{"constant":true,"inputs":[],"name":"totalSupply","outputs":[{"internalType":"uint256","name":"","type":"uint256"}],"payable":false,"stateMutability":"view","type":"function"},{"constant":true,"inputs":[{"internalType":"address","name":"","type":"address"}],"name":"userRewardPerTokenPaid","outputs":[{"internalType":"uint256","name":"","type":"uint256"}],"payable":false,"stateMutability":"view","type":"function"},{"constant":false,"inputs":[{"internalType":"uint256","name":"amount","type":"uint256"}],"name":"withdraw","outputs":[],"payable":false,"stateMutability":"nonpayable","type":"function"}]
+
+async function main() {  
+  const App = await init_ethers();
+
+  _print(`Initialized ${App.YOUR_ADDRESS}\n`);
+  _print("Reading smart contracts...\n");
+
+  const tokens = {};
+  const prices = await getMaticPrices();
+
+  const pools = SmartdexStakingContracts.map(c => { return {
+    address: c.stakingRewardAddress,
+    abi: SMARTDEX_STAKING_ABI,
+    stakeTokenFunction: "stakingToken",
+    rewardTokenFunction: "rewardsToken"
+  }})
+
+  let poolInfo = await loadMaticSynthetixPoolInfo(App, tokens, prices, pools[0].abi, pools[0].address,
+    pools[0].rewardTokenFunction, pools[0].stakeTokenFunction)
+  if(poolInfo.staked_tvl == 0){
+    _print(`Rewards start at 2021-04-20 17:00 UTC.\n`);
+  }
+
+
+  let p = await loadMultipleMaticSynthetixPools(App, tokens, prices, pools)
+  _print_bold(`Total staked: $${formatMoney(p.staked_tvl)}`);
+  if (p.totalUserStaked > 0) {
+    _print(`You are staking a total of $${formatMoney(p.totalUserStaked)} at an APR of ${(p.totalAPR * 100).toFixed(2)}%\n`);
+  }
+  
+  hideLoading();
+}
+
+const SmartdexStakingContracts =  [
+  {
+    //NIOX/USDC
+    stakingRewardAddress: '0x9c0ef78a30aa2d43f6f1aac677e227c43eb35356'
+  },
+  {
+    //WMATIC/USDC
+    stakingRewardAddress: '0x16c88e00f414436d4abbc859e5d99ec1868474af'
+  },
+  {
+    //WETH/USDC
+    stakingRewardAddress: '0xae4a735567bb1b351bebaa252c88d2ebc2fa7e56'
+  },
+  {
+    //GLQ/WETH
+    stakingRewardAddress: '0x0de7482edc5aa30e96f81642cbdad75e550c6fd4'
+  },
+  {
+    //ALOHA/NIOX
+    stakingRewardAddress: '0xaec962e533a350495c38507dd0ef1becc138af66'
+  },
+  {
+    //AGI/NIOX 
+    stakingRewardAddress: '0x9b68938a1bbe47d87cc36cfccb1f200459f4c46b'
+  }
+]
